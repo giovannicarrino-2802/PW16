562a49479b17a9d068ddaf50fb8a0bae5775ff63
giovannicarrino-2802 <giovannicarrino@outlook.com>
2026-08-22 16:35:27 +0200

    Update main.py

--- poliambulatorio-prenotazioni/backend/app/main.py
+++ poliambulatorio-prenotazioni/backend/app/main.py
@@ -19,7 +19,7 @@ Base.metadata.create_all(bind=engine)
 seed.run()   # dati di esempio, idempotente
 
 app = FastAPI(title="Piattaforma di prenotazione di visite specialistiche - API", version="1.0.0",
-              description="Project Work PW16 - Sviluppo di una applicazione full-stack API-based per"
+              description="Project Work PW16 - Sviluppo di una applicazione full-stack API-based per "
                           "un’organizzazione del settore sanitario.<br>"
                           "Applicativo gestionale per poliambulatorio: prenotazioni, "
                           "gestione medici/prestazioni/disponibilita e area "
